ecd5b79b4383c4d4c9c31af4cdb9e770aa135cc5
Angina_Anushka <136321031+A-Anushka@users.noreply.github.com>
2024-06-27 17:38:35 +0530

    universal gates

---
+++ Day 01/nor_tb.v
@@ -0,0 +1,14 @@
+module nor_gate_tb;
+reg a;
+reg b;
+wire y;
+nor_ga a1(y,a,b);
+initial 
+begin
+ #10 a=1'b0;b=1'b0;
+ #10 a=1'b0;b=1'b1;
+ #10 a=1'b1;b=1'b0;
+ #10 a=1'b1;b=1'b1;
+ #10 $stop;
+ end
+endmodule
