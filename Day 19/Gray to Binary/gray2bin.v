506a1d71a8a34d7880bf07732c5ec6b58c498c57
Angina_Anushka <136321031+A-Anushka@users.noreply.github.com>
2024-07-17 23:28:08 +0530

    Gray to Binary  code converter

---
+++ Day 19/Gray to Binary/gray2bin.v
@@ -0,0 +1,8 @@
+module gray2bin(b,g);
+    input [3:0] g;
+    output [3:0] b;
+    assign b[3]=g[3];
+    assign b[2]=b[3] ^ g[2];
+    assign b[1]=b[2] ^ g[1];
+    assign b[0]=b[1] ^ g[0];
+endmodule
