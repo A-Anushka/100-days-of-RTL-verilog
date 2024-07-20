a5db7cdb1aea14fccd99735daacebb655470453c
Angina_Anushka <136321031+A-Anushka@users.noreply.github.com>
2024-07-20 22:25:37 +0530

    Odd parity checker

--- Day 22/Odd parity checker/odd_par_checker.v
+++ Day 22/Odd parity checker/odd_par_checker.v
@@ -3,5 +3,5 @@ module odd_par_checker(par_checker,b,p);
     input [2:0] b;
     input p;
     output par_checker;
-    assign par_checker=~(b[0] ^ b[1] ^ b[2] ^p);
+    assign par_checker= ~(b[0] ^ b[1] ^ b[2] ^p);
 endmodule
